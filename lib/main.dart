0611fa060bea2e222faaf30f0853edd986848ad1
jcmaep <jcmaep@gmail.com>
2023-05-29 01:27:04 +0800

    Updated firebase tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Set debugShowCheckedModeBanner to false
+      debugShowCheckedModeBanner:
+          false, // Set debugShowCheckedModeBanner to false
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Colors.white,
@@ -39,8 +39,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController machineIDController = TextEditingController();
-  final TextEditingController prescriptionNameController = TextEditingController();
-  final TextEditingController intakeIntervalController = TextEditingController();
+  final TextEditingController prescriptionNameController =
+      TextEditingController();
+  final TextEditingController intakeIntervalController =
+      TextEditingController();
   final TextEditingController intakeTimesController = TextEditingController();
   bool isChecked = false;
 
@@ -74,87 +76,88 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Text(widget.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    )
-        ),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            )),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              controller: machineIDController,
-              decoration: InputDecoration(
-                labelText: 'Machine ID',
-                filled: true,
-                fillColor: const Color(0xFFD9D9D9),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              controller: prescriptionNameController,
-              decoration: InputDecoration(
-                labelText: 'Prescription Name',
-                filled: true,
-                fillColor: const Color(0xFFD9D9D9),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              controller: intakeIntervalController,
-              decoration: InputDecoration(
-                labelText: 'Intake Interval',
-                filled: true,
-                fillColor: const Color(0xFFD9D9D9),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              controller: intakeTimesController,
-              decoration: InputDecoration(
-                labelText: 'Intake Times',
-                filled: true,
-                fillColor: const Color(0xFFD9D9D9),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Checkbox(
-                  value: isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked = value ?? false;
-                    });
-                  },
-                  fillColor: MaterialStateColor.resolveWith(
-                        (states) => const Color(0xFF0068FF),
-                  ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: machineIDController,
+                decoration: InputDecoration(
+                  labelText: 'Machine ID',
+                  filled: true,
+                  fillColor: const Color(0xFFD9D9D9),
                 ),
-                Flexible(
-                  child: Text(
-                    'I hereby declare that the information provided in this form is complete, true, and correct to the best of my knowledge',
-                    softWrap: true,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _confirmButtonPressed,
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(const Color(0xFF0068FF)),
-                foregroundColor: MaterialStateProperty.all(Colors.white), // Set text color to white
               ),
-              child: const Text('Confirm'),
-            ),
-          ],
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: prescriptionNameController,
+                decoration: InputDecoration(
+                  labelText: 'Prescription Name',
+                  filled: true,
+                  fillColor: const Color(0xFFD9D9D9),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: intakeIntervalController,
+                decoration: InputDecoration(
+                  labelText: 'Intake Interval',
+                  filled: true,
+                  fillColor: const Color(0xFFD9D9D9),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: intakeTimesController,
+                decoration: InputDecoration(
+                  labelText: 'Intake Times',
+                  filled: true,
+                  fillColor: const Color(0xFFD9D9D9),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value ?? false;
+                      });
+                    },
+                    fillColor: MaterialStateColor.resolveWith(
+                      (states) => const Color(0xFF0068FF),
+                    ),
+                  ),
+                  Flexible(
+                    child: Text(
+                      'I hereby declare that the information provided in this form is complete, true, and correct to the best of my knowledge',
+                      softWrap: true,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _confirmButtonPressed,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(const Color(0xFF0068FF)),
+                  foregroundColor: MaterialStateProperty.all(
+                      Colors.white), // Set text color to white
+                ),
+                child: const Text('Confirm'),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
