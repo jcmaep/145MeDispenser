d4e19d3166822e0ae09b094b4551ce224605286c
jcmaep <jcmaep@gmail.com>
2023-06-14 02:20:54 +0800

    Fixing timestamp mech

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 //const bodyParser = require('body-parser')
 const SerialPort = require('serialport');
+//onst request = require('request');
+const arduinoUrl = 'http://192.168.5.2/arduino';
+//const { Timestamp } = require("firebase/compat/firestore"); // Import the Timestamp class
 
 
 // initialize firebase admin sdk
@@ -36,10 +39,20 @@ app.get("/", async (req, res) => {
 		let htmlOutput = "<html><body><h1>Prescriptions</h1>";
 		htmlOutput += "<ul>";
 		prescriptions.forEach((prescription) => {
+            // Accessing individual elements of the array
+            //const first_timestamp_array = prescription.first_timestamp;
+            const start_timestampp = prescription.start_timestamp;
+            const start_timestampp_millis = start_timestampp.toMillis();
+            const start_timestampp_date = new Date(start_timestampp_millis);
+            //const formatted_date = start_timestampp_date.toLocaleString(); // Convert to formatted date string
+            //const start_timestamppp = Date(start_timestampp_millis)
+            //Console.log processes it as normal!
+            console.log(start_timestampp_date);
 			htmlOutput += `
             <li>
                 <ul>
-                    <li>${prescription.machineID}</li>
+                    <li>${prescription}</li>
+                    <li>${start_timestampp_date}</li>
                     <li>${prescription.prescriptionName}</li>
                     <li>${prescription.intakeInterval}</li>
                     <li>${prescription.intakeTimes}</li>
@@ -56,6 +69,8 @@ app.get("/", async (req, res) => {
 	}
 });
 
+
+// Post request from Arduino, when pills are successfully dispensed.
 app.post('/test', (req, res) => {
     console.log('Received POST request from Arduino: from ' + req.ip[7] + ' with body: ' + JSON.stringify(req.body));
     console.log(req.body);
@@ -73,6 +88,20 @@ app.post('/test', (req, res) => {
     //console.log(`Input received: ${machineID}`);
     res.send(req.body);
 });
+
+//Update database
+
+// Loop to check database data, then send dispensing signal to Arduino if timestamp is == current time
+async function timestampLoop() {
+    while (true) {
+
+    }
+    //Get updated database
+    //
+}
+
+// Timestamp mechanism, push notification if missed
+
 
 // Start the server
 app.listen(port, () => {
