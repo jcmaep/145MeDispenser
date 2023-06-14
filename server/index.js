1c6284d5d8e8bc33c62b8a287d140a71e443c7ad
Angelo Convento <convento.angelo@gmail.com>
2023-06-14 13:27:37 +0800

    POST test

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const db = admin.firestore();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+var ipDict = {};
 
 app.use(express.json());
 
@@ -51,7 +52,6 @@ app.get("/", async (req, res) => {
 
 		// Send HTML response
 		res.send(htmlOutput);
-		const currentTime = Date.now();
 	} catch (error) {
 		console.error("Error fetching prescriptions:", error);
 		res.status(500).send("Error fetching prescriptions");
@@ -61,6 +61,18 @@ app.get("/", async (req, res) => {
 // Start the server
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
+});
+
+app.post("/ipaddr", (req, res) => {
+	ipAddr = req.ip[7];
+	machineID = req.body.machineID;
+	console.log(ipAddr)
+	console.log(req.ip)
+	console.log("IP address of machine: " + req.ip[7]);
+	console.log("Machine ID: " + req.body.machineID);
+
+	ipDict[machineID] = ipAddr;
+	console.log(ipDict);
 });
 
 // Post request from Arduino, when pills are successfully dispensed.
@@ -87,32 +99,29 @@ app.post("/test", (req, res) => {
 	res.send(req.body);
 });
 
-//Update database
-
 // Loop to check database data, then send dispensing signal to Arduino if timestamp is == current time
 setInterval(async () => {
 	//Get updated database
 	const snapshot = await db.collection("medicine-prescription").get();
 	const currentTime = Date.now();
-	console.log(currentTime);
+	// console.log(currentTime);
 
 	snapshot.forEach(async (doc) => {
 		const prescription = doc.data();
 		const nextIntakeTimeMillis = prescription.nextIntakeTime.toMillis();
 
 		if (currentTime >= nextIntakeTimeMillis) {
-			
 			if (prescription.intakeTimes === 0) {
 				// continue;
 			} else {
 				const lastIntakeTime = prescription.nextIntakeTime.toMillis();
-				// console.log("Last intake: " + nextIntakeTimeMillis);
-				// console.log("Interval: " + prescription.intakeInterval * 3600000);
+				console.log("Last intake: " + Date(lastIntakeTime));
+				console.log("Interval: " + prescription.intakeInterval * 3600000);
 				const newDateMillis =
 					lastIntakeTime + prescription.intakeInterval * 3600000;
 				// console.log("Next intake: " + newDateMillis);
 				// console.log(prescription.nextIntakeTime);
-	
+
 				changeTime(admin.firestore.Timestamp.fromMillis(newDateMillis), doc.id);
 				// // Arduino dispenses - START
 				// // Assuming arduino sensed a hand
